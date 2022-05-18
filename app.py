da684a04b6e0e61a23596da56590817925a17799
Thomas Skjørten <Thomas.Skjorten@visma.com>
2022-05-18 09:10:00 +0200

    Comments

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-# The Visma Busniess NXT API uses OAuth 2.0 for authentication. 
-# Two different flows are supported:
-#   - Authentication Code Flow for applications that has user interaction, this flows requires that the user logs in with a username and 
-#     password. The integration will impersonate this user when makong API-calls.
-#   - Client Credentials Flow for non-interactive integrations, this flows authenticates without requiring a user, the integration will 
-#     identify as itself and not a user. Ideal for machine-to-machine type integrations.
-# Both scenarios requires that the application/integration is known by Visma, therefore the application must be created in Visma Developer Portal
-# and request access to the Visma Business NXT API before authentication.  
+# Imports
 
-# https://docs.business.visma.net/docs/authentication/overview
-
+## Import required Flask modules
 from flask import Flask, render_template
 
+## Import OS-module to access environment-variables
+import os
+
+## Import the blueprints for the functionality in this application
+## We have chosen to use Flask blueprints so that we can separated the funtionality in smaller, more readable modules 
 from authenticate import authenticate
-# from authenticate_authlib import authenticate_authlib
 from listorders import listorders
 from getavailablecompanies import getavailablecompanies
 from countorders import countorders
@@ -20,14 +16,14 @@ from createorder import createorder
 from addorderlines import addorderlines
 from createassociate import createassociate
 
-import os
-
+# Creates the Flask application
 app = Flask(__name__)
 
+# Sets the secret key for the Flask application, to be able to store values in session
 app.secret_key = os.environ.get('SECRET_KEY') 
 
+# Register the blueprints containing all the funtionality of the application
 app.register_blueprint(authenticate)
-# app.register_blueprint(authenticate_authlib)
 app.register_blueprint(listorders)
 app.register_blueprint(getavailablecompanies)
 app.register_blueprint(countorders)
@@ -35,6 +31,8 @@ app.register_blueprint(createorder)
 app.register_blueprint(addorderlines)
 app.register_blueprint(createassociate)
 
+# Defines the default route of the application, this will serve the index-page
 @app.route("/")
 def index():
+    # Returns the template index.html
     return render_template("index.html")
